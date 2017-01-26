fdfc5cade8e79e52ef33d4459ef150004ffd9f87
Adolfo Jeritson <adolfojeritson@gmail.com>
2017-01-26 11:55:07 -0400

    jgghjvhj

--- Tarea2/testServicio.py
+++ Tarea2/testServicio.py
@@ -38,15 +38,29 @@ class ClaseServicioTest(unittest.TestCase):
         servicio = [datetime(2017, 1, 1), datetime(2017, 1, 2)]
         self.assertEquals(self.s.calcularPrecio(self.t, servicio), 0)
        
-    # Caso frontera minima de tiempo
+    # Caso borde minimo de tiempo
     def testTiempoMenos15Min(self):
         servicio = [datetime(2017, 1, 1, 7, 0), datetime(2017, 1, 1, 7, 14)]
         self.assertEquals(self.s.calcularPrecio(self.t, servicio), None)
         
-    # Caso frontera maxima de tiempo    
+    # Caso borde maximo de tiempo    
     def testTiempoMayor7Dias(self):
         servicio = [datetime(2017, 1, 1), datetime(2017, 1, 9)]
         self.assertEquals(self.s.calcularPrecio(self.t, servicio), None)
+    
+    # Caso frontera minima de tiempo
+    def testTiempo15Minutos(self):
+        self.t.set_tsem(100)
+        self.t.set_tfsem(100)
+        servicio = [datetime(2017, 1, 1, 7, 0), datetime(2017, 1, 1, 7, 15)]
+        self.assertEquals(self.s.calcularPrecio(self.t, servicio), 100)
+        
+    # Caso frontera maxima de tiempo    
+    def testTiempo7Dias(self):
+        self.t.set_tsem(100)
+        self.t.set_tfsem(100)
+        servicio = [datetime(2017, 1, 1, 0, 0), datetime(2017, 1, 8, 23, 59)]
+        self.assertEquals(self.s.calcularPrecio(self.t, servicio), 16800)
 
 if __name__ == "__main__":
     unittest.main()
