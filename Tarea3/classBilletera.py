debb8ea72854205a4db948d6b29f4b0b728160aa
Adolfo Jeritson <adolfojeritson@gmail.com>
2017-02-01 13:54:22 -0400

    Clase billetera electronica - push inicial

---
+++ Tarea3/classBilletera.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from classRecarga import Recarga
+from classConsumo import Consumo
+from sqlalchemy.sql.expression import false
+
+class Billetera:
+    
+    def __init__(self, id_b, n, a, ci, pin):
+        self.id = id_b
+        self.nombre = n + " " + a
+        self.ci = ci
+        self.pin = pin
+        self.registro_consumo = []
+        self.registro_recargas = []
+        self.saldo = 0
+        
+    def saldo(self):
+        return self.saldo
+    
+    def recargar(self, m, f, id_est):
+        r = Recarga(m, f, id_est)
+        self.registro_recargas.append(r)
+        
+    def consumir(self, pin, m, f, id_est):
+        if pin != self.pin:
+            return False
+        else:
+            if self.saldo < m:
+                return False
+            else:
+                c = Consumo(m, f, id_est)
+                self.registro_consumo.append(c)
+                self.saldo = self.saldo - m
+                return True
+                
+            
+        
+        
+        
---
+++ Tarea3/classConsumo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class Consumo:
+    
+    def __init__(self, m, f, id_est):
+        self.monto = m
+        self.fecha = f
+        self.id = id_est
+
